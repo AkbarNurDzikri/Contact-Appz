2a8376f20da91a06201f9c8a4c51a251ee1afccd
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-09-26 10:55:43 +0700

    Create contact components

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
+import ContactApp from './ContactApp';
 
 const element = <h1>Hello, world !</h1>
 
-ReactDom.createRoot(document.getElementById("root")).render(element);
+ReactDom.createRoot(document.getElementById("root")).render(<ContactApp />);
